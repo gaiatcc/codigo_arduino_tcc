dc9d59581d91be6aeff72cf8f1600b5e159f2cc4
gaiatcc <125665607+gaiatcc@users.noreply.github.com>
2023-05-19 15:04:07 -0300

    Add files via upload

---
+++ tcc/tcc.ino
@@ -0,0 +1,29 @@
+//Projeto medir a temperatura e a umidade com DHT11(sensor de umidade)
+#include <DHT.h>//Inclui a biblioteca DHT Sensor Library
+#define DHTPIN 2//Pino digital 2 conectado ao DHT11
+#define DHTTYPE DHT11//DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);//Inicializando o objeto dht do tipo DHT passando como parâmetro o pino (DHTPIN) e o tipo do sensor (DHTTYPE)
+
+void setup() {
+  Serial.begin(9600);//Inicializa a comunicação serial
+  dht.begin();//Inicializa o sensor
+}
+
+void loop() {
+  delay(2000);//Intervalo de 2 segundos entre as medições
+
+  float h = dht.readHumidity();//lê o valor da umidade e armazena na variável h do tipo float 
+  float t = dht.readTemperature();//lê o valor da temperatura e armazena na variável t do tipo float 
+
+  if (isnan(h) || isnan(t)) {//Verifica se a umidade ou temperatura são ou não um número
+    return;//Caso não seja um número retorna
+  }
+
+  Serial.print("Umidade: ");//mostra no monitor serial a mensagem "Umidade: "
+  Serial.print(h);//mostra na serial o valor da umidade
+  Serial.println("%");//mostra na serial o caractere "%" e salta para a próxima linha
+  Serial.print("Temperatura: ");//mostra no monitor serial a mensagem "Temperatura: "
+  Serial.print(t);//mostra na serial o valor da temperatura
+  Serial.println("°C ");//mostra no monitor serial "ºC" e salta para a próxima linha
+}
---
+++ tcc/tcc_rodinha/tcc_rodinha.ino
@@ -0,0 +1,48 @@
+/*Pinagem do arduino*/
+//motor_A
+int IN1 = 3 ;
+int IN2 = 4 ;
+int velocidadeA = 3;
+//motor_B
+int IN3 = 7 ;
+int IN4 = 8 ;
+int velocidadeB = 5;
+//variavel auxiliar
+int velocidade = 0;
+//Inicializa Pinos
+void setup(){
+pinMode(IN1,OUTPUT);
+pinMode(IN2,OUTPUT);
+pinMode(IN3,OUTPUT);
+pinMode(IN4,OUTPUT);
+pinMode(velocidadeA,OUTPUT);
+pinMode(velocidadeB,OUTPUT);
+}
+void loop(){
+/*Exemplo de velocidades no motor A*/
+//Sentido 2
+digitalWrite(IN1,HIGH);
+digitalWrite(IN2,LOW);
+//Alta
+analogWrite(velocidadeA,230);
+//Intermediaria
+analogWrite(velocidadeA,150);
+//Baixa
+analogWrite(velocidadeA,80);
+/*Exemplo de variacao de velocidade no motor B*/
+//Sentido 2
+digitalWrite(IN3,HIGH);
+digitalWrite(IN4,LOW);
+//velocidade de 0 a 255
+while (velocidade < 255){
+analogWrite(velocidadeB,velocidade);
+velocidade = velocidade + 10;
+delay(50);
+}
+//velocidade de 255 a 0
+while (velocidade > 0){
+analogWrite(velocidadeB,velocidade);
+velocidade = velocidade - 10;
+delay(50);
+}
+}
